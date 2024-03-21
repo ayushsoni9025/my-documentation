d614c5375b27f3b0690c6f973f4ab6c8353f7b12
ayushsoni9025 <ayushsoni9025@gmail.com>
2024-03-21 16:17:20 +0530

    Initialize my-documentation project

---
+++ src/test/java/dev/ayush/mydocymentation/MyDocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayush.mydocymentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
